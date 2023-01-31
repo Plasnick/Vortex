fcbd79b5d3424e4494d80dd419696b546714f0ad
nick mcconnell <nick129623@gmail.com>
2023-01-31 14:27:01 -0500

    show recent 5 in front end

---
+++ capstone/vue/src/views/Top5Forums.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      Most recently Active Forums:
+
+      <!-- <div class="recent5List" v-for="entry in mostRecentForums" v-bind:key> -->
+      <p>{{mostRecentForums[0]}}</p>
+      <p>{{mostRecentForums[1]}}</p>  
+      <p>{{mostRecentForums[2]}}</p>
+      <p>{{mostRecentForums[3]}}</p>
+      <p>{{mostRecentForums[4]}}</p>  
+      <!-- </div> -->
+  </div>
+</template>
+
+<script>
+import ShowRecent5 from '../services/ShowRecent5ForumsService';
+
+export default {
+    data() {
+        return { mostRecentForums : []
+        }
+    
+},
+
+created() {
+    ShowRecent5.displayTop5().then(
+        (response) => {
+            this.mostRecentForums = response.data;
+        }
+    );
+    }
+};
+</script>
+
+<style>
+html {
+    color: white;
+    background-color: black;
+}
+</style>
