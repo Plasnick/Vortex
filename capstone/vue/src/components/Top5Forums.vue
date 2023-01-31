e5c7f25e728adb5a005062689151fa74c4341fb3
Michael Krut <krutmj97@gmail.com>
2023-01-31 16:22:16 -0500

    edited top5forums component, added it into home, and created search forums component

---
+++ capstone/vue/src/components/Top5Forums.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class = "top5">
+      <h3>Top 5 Forums Today</h3>
+        <ul>
+            <li v-for="forum in mostRecentForums" v-bind:key="forum.id">
+                {{forum.forumName}}
+            </li>
+        </ul>
+      
+  </div>
+</template>
+
+<script>
+import ForumsService from '../services/ForumsService';
+
+export default {
+    data() {
+        return { mostRecentForums : []
+        }
+    
+},
+
+created() {
+    ForumsService.displayTop5().then(
+        (response) => {
+            this.mostRecentForums = response.data;
+        }
+    );
+    }
+};
+</script>
+
+<style scoped>
+/* html {
+    color: white;
+    background-color: black;
+} */
+
+li{
+    list-style: none;
+}
+</style> 
