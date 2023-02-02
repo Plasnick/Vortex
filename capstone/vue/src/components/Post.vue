e7bc9dcd2bcff022686f291d69ef69f2ffcbe8c1
Michael Krut <krutmj97@gmail.com>
2023-02-02 16:46:56 -0500

    finished post component and added it to home page

--- capstone/vue/src/components/Post.vue
+++ capstone/vue/src/components/Post.vue
@@ -2,6 +2,8 @@
   <div class="post-component">
     <div class="post-details">
       <h2>{{ post.title }}</h2>
+      <button v-on:click="upVote">Up Vote ({{ post.upVotes }})</button>
+      <button v-on:click="downVote">Down Vote ({{ post.downVotes }})</button>
       <h3>User: {{ post.userId }} Posted On: {{ post.postedAt }}</h3>
       <!-- need to get the Username from the userId. Maybe change the sql statement 
       to join the user table and select the name-->
@@ -12,28 +14,30 @@
 </template>
 
 <script>
-// import postsService from "../services/PostsService";
+ import postsService from "../services/PostsService";
 export default {
   name: "post-component",
-  //   props: ["post"],
-  //   methods: {
-  //     upVote() {
-  //       this.post.upVotes++;
-  //       postsService.updatePost(this.post).then((response) => {
-  //         if (response.status === 200) {
-  //           console.log("it worked");
-  //         }
-  //       });
-  //     },
-  //     downVote() {
-  //       this.post.downVotes++;
-  //       postsService.updatePost(this.post).then((response) => {
-  //         if (response.status === 200) {
-  //           console.log("it worked");
-  //         }
-  //       });
-  //     },
-  //   },
+     props: ["post"],
+     methods: {
+       upVote() {
+         const index = this.$store.state.posts.findIndex((element) => (element.postId === this.post.postId));
+         this.$store.state.posts[index].upVotes++
+         postsService.updatePost(this.post.postId, this.$store.state.posts[index]).then((response) => {
+           if (response.status === 200) {
+             console.log("it worked");
+           }
+         });
+       },
+       downVote() {
+         const index = this.$store.state.posts.findIndex((element) => (element.postId === this.post.postId));
+         this.$store.state.posts[index].downVotes++
+         postsService.updatePost(this.post.postId, this.$store.state.posts[index]).then((response) => {
+           if (response.status === 200) {
+             console.log("it worked");
+           }
+         });
+       },
+     },
 };
 </script>
 
