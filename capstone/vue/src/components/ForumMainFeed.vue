3f30dedf240f6a8d7f10db99bde4907aff41b591
James Weber <j.michael.weber@gmail.com>
2023-02-02 12:20:41 -0500

    fixed the individual post link and page

--- capstone/vue/src/components/ForumMainFeed.vue
+++ capstone/vue/src/components/ForumMainFeed.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="main-forum-feed">
     <div class="post" v-for="post in postsByForum" v-bind:key="post.postId">
-      <h3>{{ post.title }}</h3>
+      <router-link
+        v-bind:to="{ name: 'postAndReplies', params: { id: post.postId } }"
+      >
+        <h3>{{ post.title }}</h3></router-link
+      >
+
       <img v-if="post.img_url" :src="post.img_url" alt="Post Image" />
       <p>{{ post.postedAt }}</p>
       <p>{{ post.body }}</p>
