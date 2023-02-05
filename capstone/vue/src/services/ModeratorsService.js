06670340f7369d8b5d106266db0637787e030858
Michael Krut <krutmj97@gmail.com>
2023-02-05 14:40:43 -0500

    worked on adding moderators

--- capstone/vue/src/services/ModeratorsService.js
+++ capstone/vue/src/services/ModeratorsService.js
@@ -3,6 +3,10 @@ import axios from "axios";
 export default{
     getModeratorsForForum(forumId){
         return axios.get(`/moderators/forum/${forumId}`)
+    },
+
+    addModerator(moderator){
+        return axios.post(`/moderators`, moderator)
     }
-    
+
 }
